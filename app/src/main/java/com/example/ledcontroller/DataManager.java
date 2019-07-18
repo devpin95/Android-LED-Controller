60117c16e1a8e18a2f77d03138f161d7d16f0feb
Devin Piner <devpindroid@gmail.com>
2019-07-18 16:06:42 -0600

    Updated database to have more colors, added onUpgrade to update the database

--- app/src/main/java/com/example/ledcontroller/DataManager.java
+++ app/src/main/java/com/example/ledcontroller/DataManager.java
@@ -8,6 +8,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DataManager {
 
     private SQLiteDatabase db;
+    private String favoriteColorsTable = "favoriteColors";
 
     public DataManager(Context context) {
         SQLHelper helper = new SQLHelper(context);
@@ -23,12 +24,12 @@ public class DataManager {
 
     private class SQLHelper extends SQLiteOpenHelper {
         public SQLHelper(Context context) {
-            super(context, "color_sets", null, 1);
+            super(context, "color_sets", null, 3);
         }
 
         public void onCreate(SQLiteDatabase db) {
             String query =
-                    "CREATE TABLE favoriteColors (" +
+                    "CREATE TABLE " + favoriteColorsTable + "(" +
                             "color integer unique," +
                             "brightness integer)";
             db.execSQL(query);
@@ -37,17 +38,42 @@ public class DataManager {
             int green = 0xFF00FF00;
             int blue = 0xFF0000FF;
             int yellow = 0xFFFFFF00;
-            int orange = 0XFF8800;
+            int orange = 0XFFFF8800;
+            int purple = 0xFF720f31;
+            int blue2 = 0xFF4842DC;
+            int pink = 0xFFF94A8F;
+            int greenish = 0xFF148675;
+            int darkred = 0xFF4f0a21;
+            int pink2 = 0xFFe53978;
+            int brightblue = 0xFF52c3f1;
+            int green2 = 0xFF25a465;
+            int pink3 = 0xFFdd44f9;
+            int yellow2 = 0xFFac8820;
+            int blue3 = 0xFF0982cf;
 
-            query = "INSERT INTO favoriteColors (color, brightness) " +
+            query = "INSERT INTO " + favoriteColorsTable + " (color, brightness) " +
                     "values (" + red + "," + 100 +")," +
                     "(" + green  + ", " + 100 + ")," +
                     "(" + blue  + ", " + 100 + ")," +
                     "(" + yellow  + ", " + 100 + ")," +
+                    "(" + purple  + ", " + 100 + ")," +
+                    "(" + blue2  + ", " + 100 + ")," +
+                    "(" + pink  + ", " + 100 + ")," +
+                    "(" + greenish  + ", " + 100 + ")," +
+                    "(" + darkred  + ", " + 100 + ")," +
+                    "(" + pink2  + ", " + 100 + ")," +
+                    "(" + brightblue  + ", " + 100 + ")," +
+                    "(" + green2  + ", " + 100 + ")," +
+                    "(" + pink3  + ", " + 100 + ")," +
+                    "(" + yellow2  + ", " + 100 + ")," +
+                    "(" + blue3  + ", " + 100 + ")," +
                     "(" + orange  + ", " + 100 + ")";
             db.execSQL(query);
         }
 
-        public void onUpgrade(SQLiteDatabase db, int a, int b){ /*do nothing*/ }
+        public void onUpgrade(SQLiteDatabase db, int a, int b){
+            db.execSQL( "DROP TABLE IF EXISTS " + favoriteColorsTable );
+            onCreate(db);
+        }
     }
 }
